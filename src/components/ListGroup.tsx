c455c505ed04f846a79079df2442d335016540e7
brayokenya <kiirubrian21@gmail.com>
2023-09-27 16:44:04 +0300

    listing component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An Item</li>
+      <li className="list-group-item">An Item</li>
+      <li className="list-group-item">An Item</li>
+      <li className="list-group-item">An Item</li>
+      <li className="list-group-item">An Item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
